4f1e3271e486e1c6c6490ce1ec5a51e764161e67
Priyanthini <30474322+priyanthini@users.noreply.github.com>
2018-10-06 13:48:08 +0530

    change

--- Js/index.js
+++ Js/index.js
@@ -1,4 +1,10 @@
  // commment in js
- console.log('Hello world!,js');
- let name = "Vithu";
- console.log(name);
+// console.log('Hello world!,js');
+ //let name = "Vithu";
+ //console.log(name);
+
+ let a = 10;
+ let b = 5;
+ let c = a/b;
+ 
+ print(c);
