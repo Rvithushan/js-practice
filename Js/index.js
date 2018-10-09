857ab81e272a1da6ee8a1d68d7f54e39d611979c
Priyanthini <30474322+priyanthini@users.noreply.github.com>
2018-10-09 20:18:07 +0530

    qn1

--- Js/index.js
+++ Js/index.js
@@ -9,9 +9,19 @@
  //let name = "Vithu";
  //console.log(name);
 
- let a = 10;
- let b = 5;
- let c = a/b;
+ //let a = 1000;
+ //let b = 5;
+ //let c = a/b;
  
- console.log(c)
+ //console.log(c)
 //>>>>>>> 4f1e3271e486e1c6c6490ce1ec5a51e764161e67
+
+function factorial(x){
+    if (x === 0)
+    {
+    return 1;
+    }
+        return x * factorial (x-1);
+    }
+ print (factorial(5));
+    
