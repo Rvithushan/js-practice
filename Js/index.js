e7233b5e13c541abd7ec79feaf6515da84b487a3
vithushan <r.vithu0205@gmail.com>
2018-10-09 20:31:55 +0530

    gg

--- Js/index.js
+++ Js/index.js
@@ -1,27 +1,16 @@
  // commment in js
-//<<<<<<< HEAD
  //console.log('Hello world!,js');
  //let name = "Vithu";
- //console.log(name);
- //console.log(module);
-//=======
-// console.log('Hello world!,js');
- //let name = "Vithu";
- //console.log(name);
-
- //let a = 1000;
- //let b = 5;
- //let c = a/b;
  
- //console.log(c)
-//>>>>>>> 4f1e3271e486e1c6c6490ce1ec5a51e764161e67
+ let number = 5;
+ /*for (; number > 0; number--)
+ {
+     number = number*(number-1);
+ }
+ console.log(number);*/
+ while (number < 5)
+ {
+     sum = number * (number-1);
 
-function factorial(x){
-    if (x === 0)
-    {
-    return 1;
-    }
-        return x * factorial (x-1);
-    }
- console.log(factorial(5));
-    
+     
+ }
