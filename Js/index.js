acdc0e2aa2d4ea85f646e44bfe382e7091d12604
Priyanthini <30474322+priyanthini@users.noreply.github.com>
2018-10-09 21:07:08 +0530

    Update Js/index.js

--- Js/index.js
+++ Js/index.js
@@ -16,12 +16,18 @@
  //console.log(c)
 //>>>>>>> 4f1e3271e486e1c6c6490ce1ec5a51e764161e67
 
-function factorial(x){
-    if (x === 0)
-    {
-    return 1;
-    }
-        return x * factorial (x-1);
-    }
- console.log(factorial(5));
+//function factorial(x){
+  //  if (x === 0)
+ //   {
+   // return 1;
+    //}
+      //  return x * factorial (x-1);
+    //}
+ //console.log(factorial(5));
+
+var numarray = [15, 17, 86, 68, 13, 54, 81, 60, 26, 47, 93, 28]
+
+reverse = numarray.reverse()
+console.log(reverse)
+
     
